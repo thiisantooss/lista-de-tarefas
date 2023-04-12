8fc04acc9e3cf1458c47a03bcfa4b32e5c75f7a2
Thiago Vianna <thiago.santos101144@gmail.com>
2023-04-12 14:48:45 -0300

    Primeiro Commit da Lista de Tarefas

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    $('.cadastra').on('click', function(){
+        const nameTarefa = $('#nomeTarefa').val()
+        const li = document.createElement('li')
+        li.innerHTML = `${nameTarefa }`
+        $('ul').append(li)
+        $('#nomeTarefa').val('')
+        $(li).click(function(){
+            $(li).toggleClass('feito')
+        })
+    })
+})
